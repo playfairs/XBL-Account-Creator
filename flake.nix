6c4e5591da295947b5f91e1b6cffaf80fe2ac6d3
playfairs <root@playfairs.cc>
2025-09-28 15:08:39 -0500

    package w/ electron-forge

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  description = "Flake for Website.";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      treefmt-nix,
+      self,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        formatters =
+          (treefmt-nix.lib.evalModule pkgs (_: {
+            projectRootFile = ".git/config";
+            programs = {
+              nixfmt.enable = true;
+              nixf-diagnose.enable = true;
+              prettier.enable = true;
+            };
+          })).config.build;
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          meta = {
+            license = pkgs.lib.licenses.gpl3Plus;
+          };
+          buildInputs = with pkgs; [
+            nodejs
+            bun
+            typescript-language-server
+          ];
+
+          shellHook = ''
+            echo "This is licensed under the Unlicense"
+          '';
+        };
+
+        formatter = formatters.wrapper;
+        checks.formatting = formatters.check self;
+
+      }
+    );
+}
